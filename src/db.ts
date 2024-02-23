b52deb64a98d1d2f85105a03ae62f6ac43223400
Zetos Lima <cesar_manoel@hotmail.com>
2024-02-22 22:18:29 -0300

    fix: transactionUpdateBalance bugs & ch getBalance query

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,6 @@
 import { Pool, QueryResult } from 'pg';
 
-type UpdateResult = { bal: number; lim: number; updated: boolean };
+type UpdateResult = { bal?: number; lim?: number; updated: boolean };
 
 interface BalanceAndTransactions {
   bal: number;
@@ -34,7 +34,7 @@ const transactionUpdateBalance = async (
 ): Promise<UpdateResult> => {
   const client = await pool.connect();
 
-  if (type === 'c') {
+  if (type === 'd') {
     try {
       await client.query('BEGIN');
 
@@ -44,32 +44,30 @@ const transactionUpdateBalance = async (
         updated: boolean;
       }> = await client.query(
         `UPDATE client
-      SET bal = CASE WHEN bal - $1 >= lim * -1 THEN bal - $1 ELSE bal END
+      SET bal = bal - $1
       WHERE id = $2
-      RETURNING bal, lim, bal - $1 >= lim * -1 AS updated;
+      RETURNING bal, lim;
       `,
         [amount, clientId],
       );
 
-      const { updated } = result.rows[0];
-
-      if (updated) {
-        // Insert a new transaction record
-        await client.query(
-          `INSERT INTO transaction (cid, amount, type, descr)
+      await client.query(
+        `INSERT INTO transaction (cid, amount, type, descr)
           VALUES ($1, $2, $3, $4);
           `,
-          [clientId, amount, type, desciption],
-        );
-      }
+        [clientId, amount, type, desciption],
+      );
 
       await client.query('COMMIT');
 
-      return result.rows[0];
+      const { bal, lim } = result.rows[0];
+
+      return { bal, lim, updated: true };
     } catch (e) {
       await client.query('ROLLBACK');
-      console.error('Error, rollingback.');
-      throw e;
+      console.error('Error, rollingback.', e);
+      // throw e;
+      return { updated: false };
     } finally {
       console.log('finally');
       client.release();
@@ -113,13 +111,6 @@ const transactionUpdateBalance = async (
   }
 };
 
-// TODO: test with manual lock.
-
-//     await client.query(
-//       'BEGIN; SELECT pg_advisory_xact_lock($1); UPDATE client SET balance = balance + 100 WHERE client_id = $1; COMMIT;',
-//       [clientID],
-//     );
-
 const getBalance = async (
   clientId: number,
 ): Promise<BalanceAndTransactions> => {
@@ -128,22 +119,47 @@ const getBalance = async (
   try {
     const { rows } = await client.query(
       `WITH client_balance AS (
-       SELECT bal, lim
-       FROM client
-       WHERE id = $1
-     )
-     SELECT cb.bal, cb.lim, NOW() as current_time, json_agg(json_build_object('valor', t.amount, 'tipo', t.type, 'realizada_em', t.c_at, 'descricao', t.descr)) AS transactions
-     FROM client_balance cb
-     LEFT JOIN (
-       SELECT *
-       FROM transaction
-       WHERE cid = $1
-       ORDER BY c_at DESC
-       LIMIT 10
-     ) t ON true
-     GROUP BY cb.bal, cb.lim`,
+        SELECT bal, lim
+        FROM client
+        WHERE id = $1
+    ),
+    latest_transactions AS (
+        SELECT *
+        FROM transaction
+        WHERE cid = $1
+        ORDER BY c_at DESC
+        LIMIT 10
+    )
+    SELECT cb.bal, cb.lim, NOW() as current_time,
+           json_agg(json_build_object(
+               'valor', lt.amount,
+               'tipo', lt.type,
+               'realizada_em', lt.c_at,
+               'descricao', lt.descr
+           )) AS transactions
+    FROM client_balance cb
+    LEFT JOIN latest_transactions lt ON true
+    GROUP BY cb.bal, cb.lim;`,
       [clientId],
     );
+    // const { rows } = await client.query(
+    //   `WITH client_balance AS (
+    //    SELECT bal, lim
+    //    FROM client
+    //    WHERE id = $1
+    //  )
+    //  SELECT cb.bal, cb.lim, NOW() as current_time, json_agg(json_build_object('valor', t.amount, 'tipo', t.type, 'realizada_em', t.c_at, 'descricao', t.descr)) AS transactions
+    //  FROM client_balance cb
+    //  LEFT JOIN (
+    //    SELECT *
+    //    FROM transaction
+    //    WHERE cid = $1
+    //    ORDER BY c_at DESC
+    //    LIMIT 10
+    //  ) t ON true
+    //  GROUP BY cb.bal, cb.lim`,
+    //   [clientId],
+    // );
 
     return rows[0] as BalanceAndTransactions;
   } finally {
